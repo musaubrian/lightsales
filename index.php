b28ce1785de0e2af223b461b42a46d9fbaf8fc86
musaubrian <musaubrian15@gmail.com>
2023-10-14 08:26:09 +0300

    chore: creat landing page

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/style.css">
+    <title>LightSales</title>
+</head>
+
+<body class="bg-gradient-to-r from-orange-400 to-blue-300 min-h-screen flex items-center justify-center">
+    <div class="text-center text-gray-900 flex flex-col items-center">
+        <h1 class="text-5xl font-extrabold mb-4">Welcome to LightSales</h1>
+        <p class="text-xl text-gray-800 mb-6">Discover the perfect light fixtures to set the ambiance.</p>
+        <a href="buy" class="bg-slate-900 p-3 text-lg font-semibold text-white rounded-full hover:text-xl  hover:p-4 hover:transition-all w-3/6 ease-linear">Shop now</a>
+        <span>or</span>
+        <a href="sell" class="text-lg underline hover:decoration-dotted ease-in-out hover:transition-all">Become a seller</a>
+    </div>
+</body>
+
+</html>
---
+++ lib/connection.php
@@ -0,0 +1,7 @@
+<?php
+try {
+    $pdo = new PDO("./db/db.sqlite");
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
